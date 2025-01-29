6d8ef1a0035631ce0e3622b21dbf59f014669f6f
MarhabaRima <rima.pipadwala@outlook.com>
2025-01-29 19:25:45 +0000

    Updated folder structure

---
+++ Javascript/Arrays-Loops/Objects.js
@@ -0,0 +1,16 @@
+//Objects are used to store collections of data and 
+// more complex entities.
+
+//Example:
+const personObject = {
+    //key: value
+    name: 'Rima',
+    age: 29,
+    city: 'London',
+    travelDestination: 'South Korea'
+}
+//we have one variable called personObject
+//it has 4 key value pairs
+//it is better than storing the data in separate variables
+//accessing the values of the object
+console.log(personObject.name); //output: Rima
