8327d05c7072f882a3f1b3dca26ae52d8875dc82
MarhabaRima <rima.pipadwala@outlook.com>
2025-04-16 18:10:39 +0100

    Created react app 'my-react-app' in 'React/CFG-SecondProject'

---
+++ React/CFG-SecondProject/my-react-app/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    // 👇️ destructuring props
+    return (
+        <div className="about">
+        <h1>About Us</h1>
+        <p>This is our About page</p>
+        </div>
+    );
+}
+export default About
